24d2c78abe09200a1ddc5a04052432894c69ee26
rayanbasu <50671976+rayanbasu@users.noreply.github.com>
2021-10-11 00:10:46 +0100

    Update Forward Problem.py

--- Forward Problem.py
+++ Forward Problem.py
@@ -5,25 +5,50 @@ Created on Sun Oct 10 15:57:34 2021
 @author: rayan
 """
 
-import scipy as sp
-from scipy.signal import gaussian
-
+import numpy as np
+from numpy.random import normal
+import matplotlib.pyplot as plt
 
 #%%
-burst_duration = 150*10**-15
+burst_duration = 150
 
 
 #Energy in MeV
 energy = 14
 
 #Energy width in MeV
-energy_width = 1
-
-velocity=sp.sqrt((2*energy)/(1.67*10**-27))
+energy_width = 14
+np.sqrt((2*energy)/(1.67*10**-27))
 #%%
 
 def energy_dist():
-    dist = gaussian(10, std = energy_width)
-    return dist+energy
+    dist = normal(14, 1, 100)
+    velocity_distribution = np.sqrt((2*dist*1.67*10**-19)/(1.67*10**-27))
+    return velocity_distribution
+
+
+data = energy_dist()
+
+print(data)
+
+plt.hist(data)
+#%%
+
+position_list  = []
+
+for i in range(0, 2000):
+    
+    
+    for k in range(len(position_list)):
+        position_list[k][1] += position_list[k][0]*10**-5
+        
+    
+    
+    if i<150:
+        new_vel_dist = energy_dist()
+        for j in range(len(new_vel_dist)):
+            position_list.append([new_vel_dist[j],0])
+            
+    
     
     
