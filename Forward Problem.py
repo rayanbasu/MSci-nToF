cd93032a2bfc2a6101ebc09fd1bfdf177e6be782
rayanbasu <50671976+rayanbasu@users.noreply.github.com>
2021-10-10 16:49:04 +0100

    Update Forward Problem.py

--- Forward Problem.py
+++ Forward Problem.py
@@ -6,19 +6,24 @@ Created on Sun Oct 10 15:57:34 2021
 """
 
 import scipy as sp
-
+from scipy.signal import gaussian
 
 
 #%%
 burst_duration = 150*10**-15
 
-energy = 14*1.6*10**-19
 
-energy_width = 1*1.6*10**-19
+#Energy in MeV
+energy = 14
+
+#Energy width in MeV
+energy_width = 1
 
 velocity=sp.sqrt((2*energy)/(1.67*10**-27))
 #%%
 
-def energy_dist(temperature):
-    dist = sp.gaussian(energy, std = energy_width)
+def energy_dist():
+    dist = gaussian(10, std = energy_width)
+    return dist+energy
+    
     
