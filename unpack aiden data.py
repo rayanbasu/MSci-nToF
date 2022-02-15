804ad74b29045dbf0cd71db8a8f9e62f15c65349
eys18 <eys18@ic.ac.uk>
2022-02-15 15:02:43 +0000

    Update unpack aiden data.py

--- unpack aiden data.py
+++ unpack aiden data.py
@@ -71,7 +71,7 @@ from matplotlib import colors as mcolors
 '''
 
 #self-heating regime
-xy00 = pd.read_csv("/Users/rayanbasu/Documents/GitHub/MSci-nToF/xy00.dat"
+xy00 = pd.read_csv("/Users/ewansaw/Documents/GitHub/MSci-nToF/xy00.dat"
                    ,header = 0, delimiter='  ', engine='python')
 xy00 = xy00[['time','burn_av_Ti', 'yield_dtBHt/dt']]
 xy00 = xy00.iloc[:,0:].values
@@ -80,7 +80,7 @@ xy00[1] = 1e-3 * xy00[1] #converting eV to keV
 xy00[0] = 1e12 * xy00[0] #converting to picoseconds
 
 #ignited hotspot
-xy01 = pd.read_csv("/Users/rayanbasu/Documents/GitHub/MSci-nToF/xy01.dat"
+xy01 = pd.read_csv("/Users/ewansaw/Documents/GitHub/MSci-nToF/xy01.dat"
                    ,header = 0, delimiter='  ', engine='python')
 xy01 = xy01[['time','burn_av_Ti', 'yield_dtBHt/dt']]
 xy01 = xy01.iloc[:,0:].values
@@ -89,7 +89,7 @@ xy01[1] = 1e-3 * xy01[1] #converting eV to keV
 xy01[0] = 1e12 * xy01[0] #converting to picoseconds
 
 #propagating burn
-xy03 = pd.read_csv("/Users/rayanbasu/Documents/GitHub/MSci-nToF/xy03.dat"
+xy03 = pd.read_csv("/Users/ewansaw/Documents/GitHub/MSci-nToF/xy03.dat"
                    ,header = 0, delimiter='  ', engine='python')
 xy03 = xy03[['time','burn_av_Ti', 'yield_dtBHt/dt']]
 xy03 = xy03.iloc[:,0:].values
@@ -252,10 +252,10 @@ plt.legend()
 
 #%%
 
-Z, E_grid, t_grid = generate_source(xy00)
+Z, E_grid, t_grid = generate_source(xy03)
 
 #scale Z to make it easier to run THIS IS NOT PERMANENT
-Z /= 1e24
+Z /= 5e25
 
 #%%
 
@@ -437,13 +437,83 @@ for detector in detectors:
     skews.append(skew(skewness))
     kurts.append(kurtosis(skewness))
 
-
-plt.plot(detectors,skews, 'x')
+#%%
+'''skewness analysis'''
+plt.plot(detectors,skews,  label='Self-heating')
+plt.plot(detectors,skews1,  label='Ignited Hotspot')
+plt.plot(detectors,skews3,  label='Propagating Burn')
+plt.legend()
 plt.xlabel('detector placement (m)')
 plt.ylabel('Skewness')
 plt.grid()
 #plt.title('Constant Temperature (20 keV)')
 #plt.title('Linearly Decreasing (35 to 1 keV)')
-#plt.xlim(xmax = 2)
+plt.xlim(xmin = 0, xmax = 0.7)
+#plt.ylim(ymin = 0, ymax = 0.1)
+#%%
 
-    
+maxindex = np.unravel_index(Z.argmax(), Z.shape)
+print(maxindex)
+print(t_grid[maxindex])
+#%%
+# Plotting temp against time for different regimes
+
+#plt.plot(xy00[0], xy00[1], label='Self-heating')
+#plt.plot(xy01[0], xy01[1], label='Ignited Hotspot')
+plt.plot(xy03[0], xy03[1], label='Propagating Burn')
+plt.axvline(x=t_grid[maxindex],linestyle='--', label = 'Bang Time')
+plt.xlabel('Time (ps)')
+plt.ylabel('Burn average Ti (keV)')
+plt.legend()
+#plt.xlim(xmin =7310 ,xmax = 7325)
+#plt.xlim(xmin =8170 ,xmax = 8200)
+plt.xlim(xmin =9000 ,xmax = 9025)
+
+
+
+
+#%%
+indexmax = xy03[1].argmax()
+print(indexmax)
+
+maxtemp_time = xy03[0][indexmax]
+print(maxtemp_time)
+
+
+indexmax = xy03[2].argmax()
+print(indexmax)
+
+bang = xy03[0][indexmax]
+print(bang)
+
+#%%
+'''kurtosis analysis'''
+plt.plot(detectors,kurts,  label='Self-heating')
+plt.plot(detectors,kurts1,  label='Ignited Hotspot')
+plt.plot(detectors,kurts3,  label='Propagating Burn')
+plt.legend()
+plt.xlabel('detector placement (m)')
+plt.ylabel('Kurtosis')
+plt.grid()
+#plt.title('Constant Temperature (20 keV)')
+#plt.title('Linearly Decreasing (35 to 1 keV)')
+plt.xlim(xmin = 0, xmax = 0.5)
+#plt.ylim(ymin = 0, ymax = 0.1)
+
+
+np.save()
+
+
+#%% data
+detectors = np.linspace(0,1,20)
+detectors = np.append(detectors, [3, 5, 10, 15, 20])
+
+kurts = [0.22501401669765686, 0.1795688516613665, 0.0737452581244864, -0.03668159939530247, -0.11973655724595433, -0.1714652432075643, -0.19969058268897655, -0.21312914813496597, -0.21809207496708316, -0.2184927691770664, -0.2165896176510782, -0.2136360358091598, -0.21030823834739598, -0.20695950475885772, -0.20376500545199283, -0.20080283893951556, -0.19809906530550458, -0.195652655164547, -0.19344926005059104, -0.19146875101904026, -0.17141020617984282, -0.1691769312616187, -0.16805742981163796, -0.16779508152639844, -0.16768504260258865]
+kurts1 = [0.4718010272373272, 0.3894824627426847, 0.20457070588202964, 0.0186047951131596, -0.11479073903645132, -0.19286872631655694, -0.23171364814095874, -0.24716144815306684, -0.249978738486496, -0.24651273917256322, -0.2402510865383265, -0.2330265434377563, -0.22576159115046357, -0.21889133502330926, -0.21259512372758627, -0.20692133670644886, -0.2018540109771938, -0.19734809981745594, -0.19334785873040694, -0.1897961667229051, -0.15632944211213262, -0.15317002111038525, -0.15182275086731112, -0.1515750221547889, -0.15148933000539255]
+kurts3 = [0.4763944324262366, 0.33248648308999584, 0.06704932051165713, -0.1258514309282881, -0.21652551113075758, -0.24447809732297676, -0.24369984223513086, -0.23216516861915215, -0.21785698057456981, -0.20395388450412666, -0.19154018143160467, -0.18084002980182357, -0.1717509028866271, -0.1640671476000315, -0.15756987130184985, -0.15205995416863027, -0.14736736576554765, -0.14335107716109308, -0.13989567077493792, -0.13690718596511298, -0.11186944886390293, -0.10988236911533056, -0.10914729705678683, -0.1090483020622317, -0.109026303589006]
+
+
+skews = [-0.4294601956670631, -0.40307986068125123, -0.34247532598225183, -0.27049035393813115, -0.20431964186667903, -0.15064720440898177, -0.10956101177189607, -0.07876677854958956, -0.05571802262625284, -0.03831467625856898, -0.024994537823670403, -0.01464092815186523, -0.006465321136206284, 8.979904862042397e-05, 0.0054218165084965325, 0.009817242657093368, 0.013485348827698241, 0.0165810608423513, 0.019220615506080743, 0.021492359683102907, 0.04250615824667899, 0.04555259991198031, 0.04766629764040206, 0.048344174015896954, 0.04867875770655405]
+skews1 = [-0.4610724980609337, -0.4252863776037561, -0.346340617974957, -0.2559335438146068, -0.17622746919740187, -0.1144355027869699, -0.06932005569211142, -0.037117417076838466, -0.014194288183101715, 0.0022450509547593113, 0.014179751360336938, 0.022967725103987032, 0.029533457559052706, 0.034508752627335354, 0.038329544294876707, 0.04130034911697832, 0.043636763970317245, 0.0454935339895835, 0.04698322938003079, 0.04818880419279106, 0.05425536622321592, 0.054015736086421066, 0.05364768787853966, 0.05349791619099637, 0.0534189047066732]
+skews3 =[-0.4198065000133934, -0.3612167224750987, -0.2466533750293474, -0.1391550369446025, -0.062320865756193805, -0.012890205710694748, 0.017977310125458473, 0.037303329688534614, 0.04958408402704474, 0.05752833768676158, 0.06275359469164248, 0.0662369823819045, 0.06858040774839315, 0.07016287776163788, 0.07122833980225869, 0.07193711255942398, 0.07239659910235052, 0.07268001989173334, 0.07283808189626417, 0.07290639612794242, 0.06912051938079214, 0.06758559018726464, 0.06633070993998993, 0.06589684494729478, 0.06567744137187398]
+
